d5e5c0e13492b57e936890db0dd50529a2c48d4e
Fr0st223 <d.dejvy@gmail.com>
2017-07-11 02:24:12 +0200

    - Added side navbar support - Fixed multiple media queries
    Signed-off-by: Fr0st223 <d.dejvy@gmail.com>

--- js/navbar.js
+++ js/navbar.js
@@ -1,24 +1,43 @@
 $(document).ready(function() {
     
-
 /* Change Navbar orientation */
-    
-    
-    
-var jmediaquery = window.matchMedia("(max-width: 768px)");
-changeNavbarSide(jmediaquery);   
-jmediaquery.addListener(changeNavbarSide);   
+var mediaQuery = window.matchMedia('(max-width: 768px)');
 
-    
+  
 function changeNavbarSide() {
 
-if (jmediaquery.matches) {
-    $('.navbar-toggler').removeClass('navbar-toggler-left').addClass('navbar-toggler-right');
+if (mediaQuery.matches) {
+    $('.navbar-toggler').removeClass('navbar-toggler-left').addClass('navbar-toggler-right');  
 }
     
 else {
-     $('.navbar-toggler').removeClass('navbar-toggler-right').addClass('navbar-toggler-left'); 
+    $('.navbar-toggler').removeClass('navbar-toggler-right').addClass('navbar-toggler-left');
 }    
 }    
-        
+    
+mediaQuery.addListener(changeNavbarSide);   
+changeNavbarSide();    
+   
+
+/* Show/Hide Navbar Drawer */    
+$('#showDrawer, #showDrawer_sub').click(function(){
+    
+    $('body').css('overflow-y', 'hidden');
+    $('.main-nav').hide();    
+    $('.navbar-drawer').addClass('enableResize');
+    
+})
+    
+$('#closeDrawer').click(function(){
+    
+    $('#navbarDrawer').removeClass('enableResize');
+    $('.main-nav').show();
+    $('body').css('overflow-y', 'scroll');
+    $('.main-nav-wrapper').css('position','none');
+
+})
+    
+
+
+
 });
